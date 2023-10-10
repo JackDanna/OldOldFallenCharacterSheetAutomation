83bde99ff7735dd1c1b50481fceb3666d620f1a8
JackDanna <jackdanna78@gmail.com>
2023-10-10 09:04:22 -0400

    Splayed out more of the function call

--- FallenCharacterSheetAutomationApp/Program.fs
+++ FallenCharacterSheetAutomationApp/Program.fs
@@ -4,11 +4,33 @@ module Testing =
     open FallenLib.BuildRules
     open FallenData.Data
 
-    let javk = buildRules damageTypeData penetrationCalculationData calculatedEngageableOpponentsData 
-                   engageableOpponentsCalculationData calculatedRangeData rangeCalculationData resourceClassData
-                   attributeData  magicSkillData magicCombatData weaponClassData conduitClassData itemTierData itemData  
-                   weaponResourceClassData equipmentData skillStatData attributeStatData
-                   movementSpeedCalculationData vocationDataArray effectsTableData defenseClassData attributeDeterminedDiceModData
-                   carryWeightCalculationData weightClassData
+    let javk = 
+        buildRules 
+            damageTypeData
+            penetrationCalculationData
+            calculatedEngageableOpponentsData 
+            engageableOpponentsCalculationData
+            calculatedRangeData
+            rangeCalculationData
+            resourceClassData
+            attributeData
+            magicSkillData
+            magicCombatData
+            weaponClassData
+            conduitClassData
+            itemTierData
+            itemData  
+            weaponResourceClassData
+            equipmentData
+            skillStatData
+            attributeStatData
+            movementSpeedCalculationData 
+            vocationDataArray 
+            effectsTableData 
+            defenseClassData 
+            attributeDeterminedDiceModData
+            carryWeightCalculationData 
+            weightClassData
 
+    // Just a line for setting a breakpoint to see the results
     let x = 5
